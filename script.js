1c667e02e349b694546fb607014459fd1cf97b91
ChikChicly <chikchicly@gmail.com>
2022-09-19 18:33:20 +0700

    oLoLoL

---
+++ script.js
@@ -0,0 +1,25 @@
+// document.querySelector('#eng').style.display=`none`;
+
+for(let one=0; one<document.querySelectorAll('#eng').length; one++){
+  console.log(one);
+  document.querySelectorAll('#eng')[one].style.display=`none`;
+}
+console.log('OK');
+function checkButton(){
+  console.log('check');
+  if(document.querySelector('#che').checked){
+    console.log('Checked')
+    for(let two=0; two<document.querySelectorAll('#eng').length; two++){
+      document.querySelectorAll('#eng')[two].style.display=`block`;
+      document.querySelectorAll('#rus')[two].style.display=`none`;
+    }
+  }else{
+    for(let one=0; one<document.querySelectorAll('#eng').length; one++){
+      console.log(one);
+      document.querySelectorAll('#eng')[one].style.display=`none`;
+      document.querySelectorAll('#rus')[one].style.display=`block`;
+    }
+  }
+}
+
+document.querySelector('#che').addEventListener('click',checkButton);
